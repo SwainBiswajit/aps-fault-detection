20322334a53047310cb1d431daf015e1bc187a46
SwainBiswajit <biswajit30swain@gmail.com>
2022-12-17 12:33:10 +0530

    updated

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from sensor.pipeline.training_pipeline import start_training_pipeline
-from sensor.pipeline.batch_prediction import start_batch_prediction
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
-file_path="/config/workspace/aps_failure_training_set1.csv"
+
 print(__name__)
 if __name__=="__main__":
      try:
-          #start_training_pipeline()
-          output_file = start_batch_prediction(input_file_path=file_path)
-          print(output_file)
+          trainig_pipeline_config=config_entity.TrainingPipelineConfig()
+          data_ingestion_config=config_entity.DataIngestionConfig(training_pipeline_config=trainig_pipeline_config)
+          print((data_ingestion_config.to_dict()))
+          data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
+          data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+
+          data_validation_config=config_entity.DataValidationConfig(training_pipeline_config=trainig_pipeline_config)
+          data_validation=DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
+          data_validation_artifact=data_validation.initiate_data_validation()
+
+
      except Exception as e:
-          print(e)
+          print(e)       
